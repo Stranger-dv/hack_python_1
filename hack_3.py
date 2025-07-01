fd2f1a3facafb6277cfe60c1145b48b088835f22
Jorbi Fernandez <jorbi.fernandez0411@gmail.com>
2025-07-01 12:53:53 -0400

    feat(update) hack_3

--- hack_3.py
+++ hack_3.py
@@ -4,5 +4,6 @@ text: "fooziman" output => "Fooziman"
 
 def fn_hack_3():
     result = "fooziman"
-    #...
-    return result
+    capitalize_result = result.capitalize()
+    return capitalize_result
+
