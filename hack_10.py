77143cc6e03a5fbfa8ca9c6e2f03b7033251d400
Jorbi Fernandez <jorbi.fernandez0411@gmail.com>
2025-07-01 14:37:13 -0400

    feat(update) hack_10

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,12 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
-    return result  
+    list_result = []
+
+    new_result = result.replace("o","0").replace("i","1").replace("a","@").upper()
+
+    for letter in new_result:
+        list_result.append(letter)
+        list_result
+
+    return list_result
